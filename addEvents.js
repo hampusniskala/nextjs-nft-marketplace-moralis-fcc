d8c806c28e7afc3261ecba884743e914d68dd51c
WoodCollector <you@example.com>
2022-09-18 00:55:52 +0200

    initial commit

---
+++ addEvents.js
@@ -0,0 +1,149 @@
+const Moralis = require("moralis/node")
+require("dotenv").config()
+const contractAddresses = require("./constants/networkMapping.json")
+let chainId = process.env.chainId || 31337
+let moralisChainId = chainId == "31337" ? "1337" : chainId
+const contractAddressArray = contractAddresses[chainId]["NftMarketplace"]
+const contractAddress = contractAddressArray[contractAddressArray.length - 1]
+
+const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL
+const appId = process.env.NEXT_PUBLIC_APP_ID
+const masterKey = process.env.masterKey
+
+async function main() {
+    await Moralis.start({ serverUrl, appId, masterKey })
+    console.log(`Working with contract address ${contractAddress}`)
+
+    let itemListedOptions = {
+        chainId: moralisChainId,
+        address: contractAddress,
+        sync_historical: true,
+        topic: "ItemListed(address,address,uint256,uint256)",
+        abi: {
+            anonymous: false,
+            inputs: [
+                {
+                    indexed: true,
+                    internalType: "address",
+                    name: "seller",
+                    type: "address",
+                },
+                {
+                    indexed: true,
+                    internalType: "address",
+                    name: "nftAddress",
+                    type: "address",
+                },
+                {
+                    indexed: true,
+                    internalType: "uint256",
+                    name: "tokenId",
+                    type: "uint256",
+                },
+                {
+                    indexed: false,
+                    internalType: "uint256",
+                    name: "price",
+                    type: "uint256",
+                },
+            ],
+            name: "ItemListed",
+            type: "event",
+        },
+        tableName: "ItemListed",
+    }
+
+    let itemBoughtOptions = {
+        chainId: moralisChainId,
+        address: contractAddress,
+        sync_historical: true,
+        topic: "ItemBought(address,address,uint256,uint256)",
+        abi: {
+            anonymous: false,
+            inputs: [
+                {
+                    indexed: true,
+                    internalType: "address",
+                    name: "buyer",
+                    type: "address",
+                },
+                {
+                    indexed: true,
+                    internalType: "address",
+                    name: "nftAddress",
+                    type: "address",
+                },
+                {
+                    indexed: true,
+                    internalType: "uint256",
+                    name: "tokenId",
+                    type: "uint256",
+                },
+                {
+                    indexed: false,
+                    internalType: "uint256",
+                    name: "price",
+                    type: "uint256",
+                },
+            ],
+            name: "ItemBought",
+            type: "event",
+        },
+        tableName: "ItemBought",
+    }
+
+    let itemCanceledOptions = {
+        chainId: moralisChainId,
+        address: contractAddress,
+        topic: "ItemCanceled(address,address,uint256)",
+        sync_historical: true,
+        abi: {
+            anonymous: false,
+            inputs: [
+                {
+                    indexed: true,
+                    internalType: "address",
+                    name: "seller",
+                    type: "address",
+                },
+                {
+                    indexed: true,
+                    internalType: "address",
+                    name: "nftAddress",
+                    type: "address",
+                },
+                {
+                    indexed: true,
+                    internalType: "uint256",
+                    name: "tokenId",
+                    type: "uint256",
+                },
+            ],
+            name: "ItemCanceled",
+            type: "event",
+        },
+        tableName: "ItemCanceled",
+    }
+
+    const listedRespone = await Moralis.Cloud.run("watchContractEvent", itemListedOptions, {
+        useMasterKey: true,
+    })
+    const boughtResponse = await Moralis.Cloud.run("watchContractEvent", itemBoughtOptions, {
+        useMasterKey: true,
+    })
+    const canceledResponse = await Moralis.Cloud.run("watchContractEvent", itemCanceledOptions, {
+        useMasterKey: true,
+    })
+    if (listedRespone.success && canceledResponse.success && boughtResponse.success) {
+        console.log("Success! Database Updated with watching events")
+    } else {
+        console.log("Something went wrong...")
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
---
+++ helper-hardhat-config.js
@@ -0,0 +1,42 @@
+const { ethers } = require("hardhat")
+
+const networkConfig = {
+    4: {
+        name: "rinkeby",
+        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        subscriptionId: "8609",
+        callbackGasLimit: "500000", // 500,000
+        interval: "30",
+        mintFee: ethers.utils.parseEther("0.01"),
+    },
+    31337: {
+        name: "hardhat",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        callbackGasLimit: "500000", // 500,000
+        interval: "30",
+        mintFee: ethers.utils.parseEther("0.01"),
+    },
+    5: {
+        name: "goerli",
+        ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+        callbackGasLimit: "500000", // 500,000 gas
+        mintFee: "10000000000000000", // 0.01 ETH
+        subscriptionId: "1659", // add your ID here!
+        ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
+    },
+}
+
+const INITIAL_SUPPLY = "1000000" // 1,000,000
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    INITIAL_SUPPLY,
+}
